22397c61ea5c01025143f0cbbe3ca530df847e06
circuitmakr <circuitmakr@gmail.com>
2021-06-03 03:00:30 -0700

    scheduler started

--- src/Components/Client/C_Schedule.js
+++ src/Components/Client/C_Schedule.js
@@ -4,6 +4,13 @@ import { useEffect, useState } from "react";
 
 function Schedule() {
   const usersession = "Session Planning";
+  const today = new Date('June 5 2021');
+  const day = ['Mon', 'Tues', 'Wed', 'Thurs','Fri']
+  const handleClick = (e) =>{
+    console.log(e.target.value)
+  }
+  const daysOftheWeek = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
+  let i =0;
   return (
     <div>
       <nav>
@@ -15,12 +22,53 @@ function Schedule() {
         </div>
         <div className="s_intro_text s_tile">
           <h2>Check Availabilities</h2>
+          <div className='s_current_date'>
+          {/* <h4>{`Today is ${day[today.getDay()-1]} ${today.getMonth()+1}/${today.getDay()-1}/${today.getFullYear()}`}</h4> */}
+          <nav className ="s_calendar_nav">
+            <div className="s_calendar_btn left"></div>
+            <div className="s_calendar_btn today"><i class="fa fa-calendar-day"></i></div>
+            <div className="s_calendar_btn right"></div>
+          </nav>
+          </div>
+          <div className='s_calendar'>
+            <div className="grid-item day-1 top">{daysOftheWeek[i]} </div>
+            <div className="grid-item day-2 top">{daysOftheWeek[i+1]}</div>
+            <div className="grid-item day-3 top">{daysOftheWeek[i+2]}</div>
+            <div className="grid-item day-4 top">{daysOftheWeek[i+3]}</div>
+            <div className="grid-item day-5 top">{daysOftheWeek[i+4]}</div>
+            <div className="grid-item day-6 top">{daysOftheWeek[i+5]}</div>
+            <div className="grid-item day-4 top">{daysOftheWeek[i+6]}</div>
+            <div className="grid-item day-1 bottom">
+              <ul className='day_1'>
+                <li value='9am-10am' onClick={handleClick}>*</li>
+                <li >10am-11am</li>
+                <li >*</li>
+                <li >*</li>
+                <li >1pm-2pm</li>
+                <li >2pm-3pm</li>
+                <li >*</li>
+                <li >*</li>
+                <li onClick={handleClick} value='6pm-7pm'>*</li>
+                <li value='7pm-8pm' onClick={handleClick}>7pm-8pm</li>
+                <li value='8pm-9pm'>*</li>
+                <li>9pm-10pm</li>
+              </ul>
+            </div>
+            <div className="grid-item day-2 bottom"></div>
+            <div className="grid-item day-3 bottom"></div>
+            <div className="grid-item day-4 bottom"></div>
+            <div className="grid-item day-5 bottom"></div>
+            <div className="grid-item day-6 bottom"></div>
+            <div className="grid-item day-7 bottom"></div>
+
+            </div>
+        </div>
+
+        <div className="s_tile">
+          <h2>Session Details</h2>
         </div>
         <div className="s_tile">
-          <h2>Select a Session</h2>
-        </div>
-        <div className="s_tile">
-          <h2>Previous Sessions</h2>
+          <h2>Your Providers</h2>
         </div>
       </div>
     </div>
--- src/Components/login.js
+++ src/Components/login.js
@@ -1,27 +1,54 @@
 import "./Login.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 import { setUser } from "../redux/authReducer";
 import { useDispatch } from "react-redux";
-import {Link} from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 
 function Login() {
+const [sel_Client, setSel_Client] = useState(0)
+const [sel_Provider, setSel_Provider] = useState(0)
+let [p_logtype, setP_Logtype] = useState(false)
+let [c_logtype, setC_Logtype] = useState(false)
 
+useEffect(()=>{
+  setP_Logtype(!p_logtype)
+  console.log('use Effect!')
+},[])
+
+
+
+const provider_handleClick=()=>{
+  // setP_Logtype(!p_logtype)
+  // setC_Logtype(false);
+  setSel_Provider(!sel_Provider);
+  console.log('clicked p_log', p_logtype)
+  console.log('c_log', c_logtype)
+
+}
+const client_handleClick=()=>{
+  setC_Logtype(false);
+  setP_Logtype(false);
+  setSel_Client(!sel_Client);
+  setC_Logtype(!c_logtype);
+  console.log('clicked c_log', c_logtype)
+  console.log('p_log', p_logtype)
+}
+ 
   return (
-
     <div className="Login">
       <div className="main_container">
         <div className="app_header">
           <h2>Choose Account Type</h2>
         </div>
         <div className="account_type">
-          <button className="opt_select" id='btn_client'>
+          <button onClick={client_handleClick} className="opt_select" value="btn_client" disabled={sel_Provider}>
             <i className="fas fa-user"></i>
             <br />
             <h4>Client</h4>
           </button>
-          <button className="opt_select" id='btn_provider'>
+          <button onClick={provider_handleClick} className="opt_select" value="btn_provider" disabled={sel_Client}>
             <i className="fas fa-user-md"></i>
             <br />
             <h4>Provider</h4>
@@ -33,15 +60,18 @@ function Login() {
           <input className="password txt" placeholder="Password"></input>
         </article>
         <div className="btn_container">
-          <button className="btn_login"><Link to='/p_dashboard'>LOGIN</Link></button>
+          <button className="btn_login">
+            <Link to="/p_dashboard">LOGIN</Link>
+          </button>
           <article>or</article>
           <article className="btnNewAccount">
-            <Link to='/c_dashboard' className='signup'>CREATE AN ACCOUNT</Link>
+            <Link to="/c_dashboard" className="signup">
+              CREATE AN ACCOUNT
+            </Link>
           </article>
         </div>
       </div>
     </div>
-  
   );
 }
 
